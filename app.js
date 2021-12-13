0ee869c6bda0c3a8856e11d04f270a7c720d8455
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2021-12-13 22:52:40 +0530

    updated js file

---
+++ app.js
@@ -0,0 +1,11 @@
+const billAmt=document.querySelector("#billamount");
+// console.log(billAmt.value);
+const cashAmt=document.querySelector(".cashamount");
+const chkbtn=document.querySelector(".check-button");
+
+chkbtn.addEventListener("click",() => {
+    console.log(billAmt.value);
+    console.log(cashAmt.value);
+
+}
+);
