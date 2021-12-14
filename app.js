b2f53fca4b5a17538567d0edd947dddfc4465d7d
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2021-12-14 23:04:33 +0530

    till error msg

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const billAmt=document.querySelector("#billamount");
-// console.log(billAmt.value);
 const cashAmt=document.querySelector(".cashamount");
 const chkbtn=document.querySelector(".check-button");
+var errormsg=document.querySelector("#errorm");
+// var returnAmt;
+chkbtn.addEventListener("click",function validateReturn() {
+    errormsg.style.display ="none";
+    if(billAmt.value > 0)
+    {
+       
+        if(cashAmt.value < billAmt.value)
+        {
+            showmsg("do u want to wash the plates");
+        }
 
-chkbtn.addEventListener("click",() => {
-    console.log(billAmt.value);
-    console.log(cashAmt.value);
 
+    }//if
+    else
+    {
+
+        showmsg("BillAmt shld be greater than 0");
+
+    }//else
+    
+    // returnAmt=cashAmt-billAmt;
+});
+
+function showmsg(msg)
+{
+    errormsg.style.display ="block";
+    errormsg.innerText=msg;
 }
-);
